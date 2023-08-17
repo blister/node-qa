fdcc5c96bf2139ab58a5d6b53852cd5cf3877fa8
Eric Ryan Harrison <me@ericharrison.info>
2023-08-17 17:21:52 -0400

    Auto-run tests

--- bin/node-qa.js
+++ bin/node-qa.js
@@ -2,4 +2,23 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const {execSync} = require('child_process');
+
+const dir = path.join(process.cwd(), 'tests');
+
+fs.readdirSync(dir).forEach(file => {
+	if ( file.endsWith('.js') ) {
+		const testFilePath = path.join(dir, file);
+
+		try {
+			execSync(`node ${testFilePath}`, { stdio: 'inherit' });
+		} catch(e) {
+			console.error(`Error: ${e}`);
+			console.error(error.stderr.toString());
+		}
+	}
+});
+
 
